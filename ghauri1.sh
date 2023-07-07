43f4bbc1036ecdaeb1487bf6adbe4eb2e22a9c52
Nishant Bhagat <nishantbhagat57@gmail.com>
2023-07-07 17:38:41 +0530

    Update ghauri1.sh

--- ghauri1.sh
+++ ghauri1.sh
@@ -1,1 +1,1 @@
-cat sqli_part00.txt | rush -k -j 20 'ghauri --threads=120 --level=3 --technique=T --batch -u {}'
+cat sqli_part00.txt | rush -k -j 20 'bash -c "ghauri --threads=120 --level=3 --technique=T --batch -u {}"'
