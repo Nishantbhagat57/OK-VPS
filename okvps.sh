4f741bc8933165058ec4149bb58aec273fe29843
Nishant Bhagat <nishantbhagat57@gmail.com>
2023-07-18 23:14:48 +0530

    Update okvps.sh

--- okvps.sh
+++ okvps.sh
@@ -292,6 +292,7 @@ DNS_RESOLVER () {
     	wget https://raw.githubusercontent.com/Nishantbhagat57/OK-VPS/main/dnsreaper.json -O $HOME/.axiom/modules/dnsreaper.json
      	wget https://raw.githubusercontent.com/Nishantbhagat57/OK-VPS/main/ffuf_postprocess.json -O $HOME/.axiom/modules/ffuf_postprocess.json
       	wget https://raw.githubusercontent.com/Nishantbhagat57/OK-VPS/main/arjun1.json -O $HOME/.axiom/modules/arjun1.json
+       	wget https://raw.githubusercontent.com/Nishantbhagat57/OK-VPS/main/acunetix.json -O $HOME/.axiom/modules/acunetix.json
 
 }
 
