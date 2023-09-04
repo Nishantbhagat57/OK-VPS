3872368f9279a50b19c2ddbc3f949839d15a01c1
Nishant Bhagat <nishantbhagat57@gmail.com>
2023-09-04 18:02:59 +0530

    Update okvps.sh

--- okvps.sh
+++ okvps.sh
@@ -67,6 +67,7 @@ clear;
    	pip3 install fake-useragent
     	pip3 install static-ffmpeg
      	pip3 install selenium-recaptcha-solver
+      	pip install pytesseract
 
    	sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
 	curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
