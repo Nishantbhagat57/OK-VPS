b2f958e8912b8c284352cb01cd79246194cfd6b9
Nishant Bhagat <nishantbhagat57@gmail.com>
2023-09-06 15:33:50 +0530

    Update okvps.sh

--- okvps.sh
+++ okvps.sh
@@ -250,7 +250,7 @@ clear;
   	mkdir dorker
    	cd dorker
  	npm install puppeteer puppeteer-extra puppeteer-extra-plugin-recaptcha node-fetch fs url async puppeteer-extra-plugin-stealth unirest cheerio request-promise --save
-	
+	npm audit fix --force
 	
 	cd $HOME/BB
 	git clone https://github.com/laramies/theHarvester
