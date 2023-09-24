d69d82785c0ecb33a282f857dc29e9bffb81b7e7
Nishant Bhagat <nishantbhagat57@gmail.com>
2023-09-24 15:30:02 +0530

    Update okvps.sh

--- okvps.sh
+++ okvps.sh
@@ -335,7 +335,11 @@ clear;
 	
  	cd $HOME/BB
         git clone https://github.com/rarecoil/unwebpack-sourcemap.git
-	sudo chmod 777 cd $HOME/BB/unwebpack-sourcemap/*
+	sudo chmod 777 $HOME/BB/unwebpack-sourcemap/*
+	
+ 	cd $HOME/BB
+	git clone https://github.com/vladko312/SSTImap.git
+	sudo chmod 777 $HOME/BB/SSTImap/*
 
 	wget https://raw.githubusercontent.com/Nishantbhagat57/OK-VPS/main/mklist -O $HOME/BB/mklist/mklist
  	sudo chmod 777 $HOME/BB/mklist/*
