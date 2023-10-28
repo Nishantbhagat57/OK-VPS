55698dde2e66f9d91d00bac392c15b84713bfea2
Nishant Bhagat <nishantbhagat57@gmail.com>
2023-10-28 22:43:14 +0530

    Update okvps.sh

--- okvps.sh
+++ okvps.sh
@@ -408,7 +408,8 @@ mkdir -p $HOME/BB/wordlist
       	wget https://raw.githubusercontent.com/Nishantbhagat57/OK-VPS/main/bxss.json -O $HOME/.axiom/modules/bxss.json
       	wget https://raw.githubusercontent.com/Nishantbhagat57/OK-VPS/main/katana1.json -O $HOME/.axiom/modules/katana1.json
        	wget https://raw.githubusercontent.com/Nishantbhagat57/OK-VPS/main/httpx1.json -O $HOME/.axiom/modules/httpx1.json
-	
+	wget https://raw.githubusercontent.com/Nishantbhagat57/OK-VPS/main/dnsresolver.json -O $HOME/.axiom/modules/dnsresolver.json
+ 
       	sudo chmod 777 $HOME/.axiom/modules/*
      	cd $HOME/BB
 	git clone https://github.com/Nishantbhagat57/jsleaks.git
