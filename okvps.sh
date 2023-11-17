08d882a20db043b0065f9ca08e847634c38a6158
Nishant Bhagat <nishantbhagat57@gmail.com>
2023-11-17 22:24:21 +0530

    Update okvps.sh

--- okvps.sh
+++ okvps.sh
@@ -82,6 +82,7 @@ mkdir -p $HOME/BB/wordlist
     	pip3 install url-normalize
      	pip3 install fuzzywuzzy
       	pip3 install pandas
+       	pip3 install python-Levenshtein
 
    	sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
 	curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
