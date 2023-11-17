9da017117e87b6d14601971d682e0e6580c81caf
Nishant Bhagat <nishantbhagat57@gmail.com>
2023-11-17 22:33:19 +0530

    Update okvps.sh

--- okvps.sh
+++ okvps.sh
@@ -84,6 +84,7 @@ mkdir -p $HOME/BB/wordlist
       	pip3 install pandas
        	pip3 install python-Levenshtein
 	pip3 install datasketch
+ 	pip3 install pygtrie
 
    	sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
 	curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
