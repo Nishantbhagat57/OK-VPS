0038445187e411722c45d16d3c16cac9b24ab44f
Nishant Bhagat <nishantbhagat57@gmail.com>
2024-01-22 18:26:42 +0530

    Update okvps.sh

--- okvps.sh
+++ okvps.sh
@@ -288,8 +288,6 @@ mkdir -p $HOME/BB/wordlist
    	cd $HOME/BB/dorker
     	npm install axios puppeteer puppeteer-extra puppeteer-extra-plugin-recaptcha node-fetch fs url async puppeteer-extra-plugin-stealth unirest cheerio request-promise request request-promise-native path fs string-similarity axios --save
 	npm audit fix --force
-	npm install axios puppeteer puppeteer-extra puppeteer-extra-plugin-recaptcha node-fetch fs url async puppeteer-extra-plugin-stealth unirest cheerio request-promise path request request-promise-native fs string-similarity axios --save
- 	npm audit fix --force
   	sudo chmod 777 *
   
 	cd $HOME/BB
