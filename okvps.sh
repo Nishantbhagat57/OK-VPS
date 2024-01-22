b49bfb4ba7bc4fe372fdc18eb93a2662e7fb364d
Nishant Bhagat <nishantbhagat57@gmail.com>
2024-01-22 15:42:03 +0530

    Update okvps.sh

--- okvps.sh
+++ okvps.sh
@@ -224,6 +224,9 @@ mkdir -p $HOME/BB/wordlist
 
    	go install github.com/ffuf/pencode/cmd/pencode@latest
   	sudo cp $HOME/go/bin/pencode /usr/local/bin
+	
+	go install github.com/BishopFox/jsluice/cmd/jsluice@latest
+	sudo cp $HOME/go/bin/jsluice /usr/local/bin
   
   	cd $HOME/BB
  	git clone https://github.com/Hackmanit/Web-Cache-Vulnerability-Scanner.git
