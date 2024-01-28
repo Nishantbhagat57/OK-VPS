1a12a0c00452b9b3ad0b3ac88977b92aeea7fc07
Nishant Bhagat <nishantbhagat57@gmail.com>
2024-01-28 15:37:52 +0530

    Update savedb.py

--- savedb.py
+++ savedb.py
@@ -1,29 +1,60 @@
 import sqlite3
 import sys
 import os
-import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 from datetime import datetime
 import random
 import string
 from fake_useragent import UserAgent
+from concurrent.futures import ThreadPoolExecutor
+import argparse
+import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+import queue
+
+# Initialize user agent
+user_agent = UserAgent()
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+retry_strategy = Retry(
+    total=3,
+    backoff_factor=1,
+    status_forcelist=[429, 503, 504],
+    allowed_methods=["HEAD", "GET", "OPTIONS"]
+)
+adapter = HTTPAdapter(max_retries=retry_strategy)
 
 # Initialize it outside your get_response function
 user_agent = UserAgent(browsers=['firefox', 'chrome'])
 
 def get_response(url):
-    try:
-        headers = {"User-Agent": user_agent.random}
-        response = requests.get(url, headers=headers, timeout=15)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        content = soup.prettify()
-        content_length = len(content)
-        status_code = response.status_code
-        return status_code, content_length, content
-    except Exception as ex:
-        print(f"Exception with URL: {url} - {str(ex)}")
-        return None, None, None
+    session = requests.Session()
+    session.verify = False
+    session.mount("https://", adapter)
+    session.mount("http://", adapter)
+
+    for _ in range(3):  # retry 3 times
+        try:
+            headers = {"User-Agent": user_agent.random}
+            response = session.get(url, headers=headers, timeout=15)
+            
+            # only retry for error status codes 429, 503 and 504
+            if response.status_code in [429, 503, 504]:
+                print(f"Attempt failed with status code {response.status_code}. Retrying URL: {url}")
+                continue
+
+            soup = BeautifulSoup(response.text, 'html.parser')
+            content = soup.prettify()
+            content_length = len(content)
+            return response.status_code, content_length, content
+
+        except Exception as ex:
+            print(f"Failed fetching URL: {url}. Error: {str(ex)}")
+    return None, None, None
 
 def create_database(db_path):
     conn = sqlite3.connect(db_path)
@@ -40,25 +71,43 @@ def create_database(db_path):
     conn.commit()
     return conn
 
-def process_urls(file_path, output_dir):
-    with open(file_path) as f:
+def process_url(results_queue, url):
+    url = url.strip()
+    print(f"Processing URL: {url}")
+    domain = urlparse(url).hostname
+    status_code, content_length, content = get_response(url)
+    if status_code is None:
+        print(f"Skipping URL: {url} due to error during fetching.")
+        return
+
+    results_queue.put((domain, url, status_code, content_length, content))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_file", help="Path to input file")
+    parser.add_argument("output_dir", help="Path to output directory")
+    parser.add_argument("-t", "--threads", type=int, default=20, help="Number of concurrent threads")
+
+    args = parser.parse_args()
+
+    with open(args.input_file) as f:
         urls = f.read().splitlines()
+        print(f"Found {len(urls)} URLs. Starting process...")
 
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     rand_str = ''.join(random.choices(string.ascii_lowercase + string.digits, k=5))
-    db_name = f"{output_dir}/database_{rand_str}_{timestamp}.db"
+    db_name = f"{args.output_dir}/database_{rand_str}_{timestamp}.db"
 
     db = create_database(db_name)
 
-    for url in urls:
-        url = url.strip()
-        print(f"Processing URL: {url}")
-        domain = urlparse(url).hostname
-        status_code, content_length, content = get_response(url)
-        if status_code is None:
-            print(f"Skipping URL: {url} due to error during fetching.")
-            continue
+    results_queue = queue.Queue()
 
+    with ThreadPoolExecutor(max_workers=args.threads) as executor:
+        for url in urls:
+            executor.submit(process_url, results_queue, url)
+
+    while not results_queue.empty():
+        domain, url, status_code, content_length, content = results_queue.get()
         db.cursor().execute(
             'INSERT INTO url_info (domain, url, status, content_length, content) VALUES(?,?,?,?,?)',
             (domain, url, status_code, content_length, content)
@@ -66,10 +115,3 @@ def process_urls(file_path, output_dir):
         db.commit()
 
     db.close()
-
-if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print(f"Usage: python {sys.argv[0]} [input file path] [output directory path]")
-        sys.exit(1)
-
-    process_urls(sys.argv[1], sys.argv[2])
