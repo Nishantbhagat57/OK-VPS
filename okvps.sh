acbbaa932bb638ebef603c09f4c645bbb6d3e9f6
Nishant Bhagat <nishantbhagat57@gmail.com>
2024-02-10 22:15:37 +0530

    Update okvps.sh

--- okvps.sh
+++ okvps.sh
@@ -89,7 +89,8 @@ mkdir -p $HOME/BB/wordlist
  	pip3 install pygtrie
   	pip3 install scikit-learn
    	pip3 install pyOpenSSL
-
+	pip3 install ndg-httpsclient
+ 
    	sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
 	curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
