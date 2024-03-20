7445f4787592c1cd96fbfd1e5116a0a4085ddc7e
Nishant Bhagat <nishantbhagat57@gmail.com>
2024-03-21 00:34:12 +0530

    Update okvps.sh

--- okvps.sh
+++ okvps.sh
@@ -111,6 +111,7 @@ mkdir -p $HOME/BB/wordlist
   	pip3 install tldextract
    	pip3 install tqdm
     	pip3 install pandas
+     	pip3 install aiofile
  
    	sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
 	curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
