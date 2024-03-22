6519e6ec689c8f41fb1484af8716c4f079540e5d
Nishant Bhagat <nishantbhagat57@gmail.com>
2024-03-22 22:25:29 +0530

    Update okvps.sh

--- okvps.sh
+++ okvps.sh
@@ -113,6 +113,7 @@ mkdir -p $HOME/BB/wordlist
     	pip3 install pandas
      	pip3 install aiofile
       	pip3 install pybloom-live
+       	pip3 install cython
        	pip3 install pybloomfiltermmap3
  
    	sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
