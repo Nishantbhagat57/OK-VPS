763796ab1a7ab2dbe53af28948f7fcb7a1b46ee0
Nishant Bhagat <nishantbhagat57@gmail.com>
2024-05-31 23:27:15 +0530

    Update ghauri8.sh

--- ghauri8.sh
+++ ghauri8.sh
@@ -1,1 +1,1 @@
-cat sqli_part07.txt | rush -k -j 8 'bash -c "ghauri --url \"{}\" --confirm --timeout 20 --threads 40 --level 3 --flush-session --fresh-queries --technique BT --batch"'
+cat sqli_part07.txt | rush -k -j 8 'bash -c "ghauri --url \"{}\" --confirm --timeout 30 --threads 40 --level 3 --flush-session --fresh-queries --technique BT --batch"'
