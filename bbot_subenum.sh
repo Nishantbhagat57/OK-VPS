442cd18b8ee3098b9398571ca1ddd9293e421ca9
Nishant Bhagat <nishantbhagat57@gmail.com>
2024-06-27 20:01:09 +0530

    Update bbot_subenum.sh

--- bbot_subenum.sh
+++ bbot_subenum.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 expect -c "
 set timeout -1
-spawn bbot -t $1 -f subdomain-enum --force
+spawn bbot -t $1 -f subdomain-enum --force --ignore-failed-deps
 expect \"Scan ready. Press enter to execute\" {
     send \"\r\"
 }
