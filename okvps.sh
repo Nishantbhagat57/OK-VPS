f59eac327ccbc95c0df1963b13317d18ca141822
Nishant Bhagat <nishantbhagat57@gmail.com>
2025-04-18 21:47:34 +0530

    Update okvps.sh

--- okvps.sh
+++ okvps.sh
@@ -330,16 +330,31 @@ mkdir -p $HOME/BB/wordlist
 	sudo ln -s /home/linuxbrew/.linuxbrew/Cellar/python@3.11/3.11.*/bin/wheel3.11 wheel
 	sudo ln -s /home/linuxbrew/.linuxbrew/Cellar/python@3.11/3.11.*/bin/2to3-3.11 2to3
  
+ 	sudo apt-get update && sudo apt-get clean && sudo apt-get install -y build-essential
+	sudo apt-get update -y
+	sudo apt-get install ca-certificates curl -y
+	sudo install -m 0755 -d /etc/apt/keyrings
+	sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+	sudo chmod a+r /etc/apt/keyrings/docker.asc
+	
+	# Add the repository to Apt sources:
+	echo \
+	  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+	  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+	  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+	sudo apt-get update -y
+	
+	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
  
- 
-   	sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
-	curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-	sudo apt update -y
-	sudo apt-get install docker-ce docker-ce-cli containerd.io -y
- 	sudo usermod -aG docker ${USER}
-  	sudo docker pull ghcr.io/praetorian-inc/noseyparker:latest
-   	sudo apt update && sudo apt -y install font-manager
+	#sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
+	#curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+	#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+	#sudo apt update -y
+	#sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+	#sudo usermod -aG docker ${USER}
+	#sudo docker pull ghcr.io/praetorian-inc/noseyparker:latest
+	#sudo apt update && sudo apt -y install font-manager
   	
 
 	#MassDNS
