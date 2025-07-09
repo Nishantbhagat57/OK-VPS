fd84788e1877ecaaba606a9db586089b66e61d64
Nishant Bhagat <nishantbhagat57@gmail.com>
2025-07-09 21:09:21 +0530

    Update okvps.sh

--- okvps.sh
+++ okvps.sh
@@ -300,6 +300,7 @@ mkdir -p $HOME/BB/wordlist
    	pip3 install bbot
 	pip3 install cryptg
  	pip3 install PyExecJS
+  	pip3 install px-proxy
   	pip3 install git+https://github.com/mitmproxy/netlib.git@master
   	pip3 install --upgrade subdominator
     	bbot --help --ignore-failed-deps
